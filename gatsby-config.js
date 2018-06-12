316b6af592313e28c8b6206e3671319b5e433260
Andy Tran <andy8tran@gmail.com>
2018-06-12 14:16:16 -0400

    Edit metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: 'Gatsby Starter - Strata by HTML5 UP',
-        author: 'Hunter Chang',
-        description: 'A Gatsby.js Starter based on Strata by HTML5 UP',
+        title: 'Andy Tran',
+        author: 'Andy Tran',
+        description: 'Andy Tran',
     },
     pathPrefix: '/',
     plugins: [
